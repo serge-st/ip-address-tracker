76125650ade172d4af5776dd2369c72ca51d2e61
Serge Stecenko <sergejs.stecenko@gmail.com>
2022-11-07 19:04:33 +0200

    complete map

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import './style.css'
 import { FC } from 'react';
+import { observer } from 'mobx-react-lite';
 import Header from './components/Header';
+import IpMap from './components/Map/IpMap';
+import store from './store/store';
 
-const App: FC = () => {
+const App: FC = observer(() => {
     return (
         <div className='app'>
             <Header />
+            <IpMap isLoading={store.geoIpStore.isLoading} />
             <div className="attribution">
-                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-                Coded by <a href="https://github.com/serge-st/ip-address-tracker">Serge St</a>.
+                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
+                Coded by <a href="https://github.com/serge-st/ip-address-tracker" target="_blank" rel="noreferrer">Serge St</a>.
             </div>
         </div>
     );
-};
+});
 
 export default App;
---
+++ src/components/Map/IpMap.tsx
@@ -0,0 +1,65 @@
+import { observer } from 'mobx-react-lite';
+import { FC, useEffect, useState } from "react";
+import './IpMap.css';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import { Icon, LatLngTuple } from 'leaflet'
+import 'leaflet/dist/leaflet.css';
+import store from "../../store/store";
+
+interface IpMapProps {
+    isLoading: boolean;
+}
+
+const ChangeView: FC = observer(() => {
+    const map = useMap();
+    if (store.geoIpStore.data?.location) {
+        const {lat, lng} = store.geoIpStore.data?.location;
+        map.setView([lat, lng]);
+    }
+    return null
+})
+
+const IpMap: FC<IpMapProps> = observer(({isLoading}) => {
+    const [position, setPosition] = useState<LatLngTuple | undefined>(undefined);
+    const customIcon = new Icon({
+        iconUrl: "/icon-location.svg",
+        iconSize: [23, 28],
+    });
+
+    useEffect(() => {
+        if (!store.geoIpStore.isLoading && store.geoIpStore.data?.location) {
+            const {lat, lng} = store.geoIpStore.data?.location;
+            setPosition([lat, lng])
+        }
+    }, [isLoading]);
+
+    return (
+        <div id='map'>
+            {position
+                ?
+                <MapContainer
+                    center={position} 
+                    zoom={11} 
+                    scrollWheelZoom={false}
+                >
+                    <ChangeView />
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker 
+                        position={position}
+                        icon={customIcon}
+                    >
+                        <Popup>
+                            Your Approximate Location
+                        </Popup>
+                    </Marker>
+                </MapContainer>                     
+                : null
+            }
+        </div>
+    );
+});
+
+export default IpMap;
